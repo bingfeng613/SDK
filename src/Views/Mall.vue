8aa2fc870a3a3a182c8c18ac181d1295fd47a0ff
bingfeng613 <2546414896@qq.com>
2024-08-03 16:46:50 +0800

    实现已解析库 接口调用

--- src/Views/Mall.vue
+++ src/Views/Mall.vue
@@ -3,26 +3,27 @@
     <h1>P-Sticker:SDK隐私声明合规检测平台</h1>
     <div class="actions">
       <el-checkbox v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
-      <el-input v-model="search" placeholder="搜索" prefix-icon="el-icon-search" class="search-input"></el-input>
-      <el-button type="primary" @click="deleteSelected">删除</el-button>
+      <el-input v-model="search" placeholder="搜索" prefix-icon="el-icon-search" class="search-input"
+        @input="handleSearch"></el-input>
+      <el-button type="primary" @click="confirmDelete">删除</el-button>
       <el-button type="primary" @click="downloadOriginal">下载原文</el-button>
       <el-button type="primary" @click="exportData">导出数据</el-button>
       <el-button type="primary" @click="generateStats">生成统计</el-button>
     </div>
-    <el-table v-loading="loading" :data="filteredData" style="width: 100%" @selection-change="handleSelectionChange"
+    <el-table v-loading="loading" :data="tableData" style="width: 100%" @selection-change="handleSelectionChange"
       ref="table">
       <el-table-column type="selection" width="55"></el-table-column>
       <el-table-column prop="appName" label="APP名称" width="150"></el-table-column>
-      <el-table-column prop="missingStatements" label="缺失声明数" width="150"></el-table-column>
-      <el-table-column prop="missingItems" label="缺失声明项" width="150"></el-table-column>
-      <el-table-column prop="redundantStatements" label="模糊声明项" width="150"></el-table-column>
-      <el-table-column prop="redundantItems" label="模糊声明项数" width="150"></el-table-column>
-      <el-table-column prop="invalidLinks" label="无效链接数" width="150"></el-table-column>
-      <el-table-column prop="invalidLinkItems" label="无效链接项" width="300"></el-table-column>
+      <el-table-column prop="lackDataNum" label="缺失声明数" width="150"></el-table-column>
+      <el-table-column prop="lackData" label="缺失声明项" width="150"></el-table-column>
+      <el-table-column prop="fuzzyDataNum" label="模糊声明项" width="150"></el-table-column>
+      <el-table-column prop="fuzzyData" label="模糊声明项数" width="150"></el-table-column>
+      <el-table-column prop="brokenLinkNum" label="无效链接数" width="150"></el-table-column>
+      <el-table-column prop="brokenLink" label="无效链接项" width="300"></el-table-column>
     </el-table>
     <div class="pagination">
       <el-pagination @current-change="handlePageChange" :current-page="currentPage" :page-size="pageSize"
-        layout="prev, pager, next" :total="filteredData.length"></el-pagination>
+        layout="prev, pager, next" :total="total"></el-pagination>
     </div>
   </div>
 </template>
@@ -30,6 +31,8 @@
 <script>
 import { export_json_to_excel } from './Export2Excel';
 import { mapActions } from 'vuex';
+import axios from 'axios';
+import { MessageBox } from 'element-ui';
 
 export default {
   data() {
@@ -38,46 +41,11 @@ export default {
       search: '',
       loading: false,
       currentPage: 1,
-      pageSize: 10,
+      pageSize: 5,
+      total: 0,
       selectedItems: [],
-      tableData: [
-        {
-          appName: '微信',
-          missingStatements: 7,
-          missingItems: 'IP地址信息',
-          redundantStatements: 2,
-          redundantItems: '网络信息',
-          invalidLinks: 5,
-          invalidLinkItems: 'https://www.umeng.com'
-        },
-        {
-          appName: 'QQ',
-          missingStatements: 5,
-          missingItems: '机型信息',
-          redundantStatements: 6,
-          redundantItems: '网页浏览记录',
-          invalidLinks: 7,
-          invalidLinkItems: 'https://www.talkingdata.com'
-        },
-        {
-          appName: '陌陌',
-          missingStatements: 9,
-          missingItems: '系统版本信息',
-          redundantStatements: 3,
-          redundantItems: '传感器信息',
-          invalidLinks: 4,
-          invalidLinkItems: 'https://support.im'
-        },
-        // Add more sample data as needed
-      ],
+      tableData: []
     };
-  },
-  computed: {
-    filteredData() {
-      return this.tableData.filter(item =>
-        item.appName.includes(this.search)
-      );
-    }
   },
   methods: {
     ...mapActions(['updateStats']),
@@ -87,12 +55,116 @@ export default {
     handleSelectionChange(val) {
       this.selectedItems = val;
     },
+    // 获取数据
+    async fetchData() {
+      this.loading = true;
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/apps/list', {
+          params: {
+            page_size: this.pageSize,
+            page: this.currentPage
+          }
+        });
+        this.tableData = response.data.results;
+        this.total = response.data.count;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        this.loading = false;
+      }
+    },
+    // 字段查询 获取数据
+    async searchApps() {
+      this.loading = true;
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/apps/search', {
+          params: {
+            keyword: this.search,
+            page_size: this.pageSize,
+            page: this.currentPage
+          }
+        });
+        this.tableData = response.data.results;
+        this.total = response.data.count;
+      } catch (error) {
+        console.error('Error searching apps:', error);
+      } finally {
+        this.loading = false;
+      }
+    },
+    handleSearch() {
+      this.currentPage = 1; // Reset to the first page when searching
+      this.searchApps();
+    },
+    // 删除数据
+    confirmDelete() {
+      if (this.selectedItems.length === 0) {
+        this.$message.warning('请先选择要删除的数据');
+        return;
+      }
+      MessageBox.confirm('确认是否删除?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteSelected();
+      }).catch(() => {
+        this.$message.info('已取消删除');
+      });
+    },
     deleteSelected() {
-      // 删除功能的实现
+      const idsToDelete = this.selectedItems.map(item => item.id);
+
+      axios.delete('http://127.0.0.1:8000/apps/delete/', { data: { ids: idsToDelete } })
+        .then(response => {
+          if (response.status === 200) {
+            this.$message.success('删除成功');
+            this.fetchData();
+          } else {
+            this.$message.error('删除失败，请重试');
+          }
+        })
+        .catch(error => {
+          this.$message.error('删除过程中出现错误，请重试');
+          console.error('Error deleting items:', error);
+        });
     },
+
     downloadOriginal() {
-      // 下载功能的实现
+      if (this.selectedItems.length === 0) {
+        this.$message.warning('请先选择要下载的内容');
+        return;
+      }
+
+      const idsToDownload = this.selectedItems.map(item => item.id);
+
+      // Fetch the content of the selected items
+      const downloadPromises = this.selectedItems.map(item =>
+        axios.get(item.htmlUrl, { responseType: 'blob' })
+      );
+
+      // Wait for all the requests to complete
+      Promise.all(downloadPromises)
+        .then(responses => {
+          responses.forEach((response, index) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `${this.selectedItems[index].appName}_privacy_policy.html`);
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+          });
+          this.$message.success('下载成功');
+        })
+        .catch(error => {
+          this.$message.error('下载过程中出现错误，请重试');
+          console.error('Error downloading items:', error);
+        });
+
+
     },
+    // 导出数据
     exportData() {
       if (this.selectedItems.length === 0) {
         this.$message.warning('请先选择要导出的数据');
@@ -100,14 +172,15 @@ export default {
       }
       const data = this.selectedItems.map(item => ({
         'APP名称': item.appName,
-        '缺失声明数': item.missingStatements,
-        '缺失声明项': item.missingItems,
-        '模糊声明项': item.redundantStatements,
-        '模糊声明项数': item.redundantItems,
-        '无效链接数': item.invalidLinks,
-        '无效链接项': item.invalidLinkItems
+        '缺失声明数': item.lackDataNum,
+        '缺失声明项': item.lackData,
+        '模糊声明项': item.fuzzyDataNum,
+        '模糊声明项数': item.fuzzyData,
+        '无效链接数': item.brokenLinkNum,
+        '无效链接项': item.brokenLink,
+        '隐私政策原文链接': item.htmlUrl
       }));
-      export_json_to_excel(['APP名称', '缺失声明数', '缺失声明项', '模糊声明项', '模糊声明项数', '无效链接数', '无效链接项'], data, '导出数据');
+      export_json_to_excel(['APP名称', '缺失声明数', '缺失声明项', '模糊声明项', '模糊声明项数', '无效链接数', '无效链接项', '隐私政策原文链接'], data, '导出数据');
     },
     generateStats() {
       if (this.selectedItems.length === 0) {
@@ -115,21 +188,19 @@ export default {
         return;
       }
 
-      // 统计选中数据的各种信息
       const linkCounts = { 有效: 0, 无法访问: 0, 非隐私政策: 0, APP隐私政策: 0, SDK隐私政策: 0 };
       const complianceCounts = { 合规: 0, 缺失声明: 0, 模糊声明: 0 };
 
       this.selectedItems.forEach(item => {
-        // 示例统计逻辑，根据实际数据需要调整
-        linkCounts['有效'] += item.invalidLinks === 0 ? 1 : 0;
-        linkCounts['无法访问'] += item.invalidLinks > 0 ? 1 : 0;
-        linkCounts['非隐私政策'] += item.invalidLinkItems.includes('非隐私政策') ? 1 : 0;
-        linkCounts['APP隐私政策'] += item.invalidLinkItems.includes('APP隐私政策') ? 1 : 0;
-        linkCounts['SDK隐私政策'] += item.invalidLinkItems.includes('SDK隐私政策') ? 1 : 0;
+        linkCounts['有效'] += item.brokenLinkNum === 0 ? 1 : 0;
+        linkCounts['无法访问'] += item.brokenLinkNum > 0 ? 1 : 0;
+        linkCounts['非隐私政策'] += item.brokenLink.includes('非隐私政策') ? 1 : 0;
+        linkCounts['APP隐私政策'] += item.brokenLink.includes('APP隐私政策') ? 1 : 0;
+        linkCounts['SDK隐私政策'] += item.brokenLink.includes('SDK隐私政策') ? 1 : 0;
 
-        complianceCounts['合规'] += item.missingStatements === 0 ? 1 : 0;
-        complianceCounts['缺失声明'] += item.missingStatements > 0 ? 1 : 0;
-        complianceCounts['模糊声明'] += item.redundantStatements > 0 ? 1 : 0;
+        complianceCounts['合规'] += item.lackDataNum === 0 ? 1 : 0;
+        complianceCounts['缺失声明'] += item.lackDataNum > 0 ? 1 : 0;
+        complianceCounts['模糊声明'] += item.fuzzyDataNum > 0 ? 1 : 0;
       });
 
       const linkData = {
@@ -148,16 +219,21 @@ export default {
         }]
       };
 
-      // 更新统计信息
       this.updateStats({ linkData, complianceData });
-
-      // 跳转到 User 页面
       this.$router.push({ name: 'user' });
     },
     handlePageChange(page) {
       this.currentPage = page;
-    },
+      if (this.search) {
+        this.searchApps();
+      } else {
+        this.fetchData();
+      }
+    }
   },
+  mounted() {
+    this.fetchData();
+  }
 };
 </script>
 
